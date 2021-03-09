739aaf0d1ae2e46588e84b8e2f5e451cec3cb349
amardahm <amardahmani111@gmail.com>
2021-03-09 20:41:40 +0100

    created models for posts app

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,29 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Post(models.Model):
+    publisher = models.ForeignKey(User,on_delete=models.CASCADE)
+    caption = models.CharField(max_length=100)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.caption
+
+class postImage(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    image = models.ImageField()
+
+class comments(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
+    content = models.CharField(max_length=100,blank=False)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    data_created = models.DateTimeField(default=timezone.now())
+
+
+class like(models.Model):
+    liker = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now())
 
